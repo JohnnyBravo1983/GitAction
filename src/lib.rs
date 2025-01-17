f657cede2b72c877c46bfd279b6e3d9069005162
Johnny Strømø <113671758+JohnnyBravo1983@users.noreply.github.com>
2025-01-17 16:11:43 +0100

    change in lib.rs and ci.yml

--- src/lib.rs
+++ src/lib.rs
@@ -4,23 +4,16 @@ use polars::prelude::*;
 /// Summerer tallene i alle numeriske kolonner fra en nested list
 #[pyfunction]
 pub fn sum_numeric_columns(data: Vec<Vec<f64>>) -> PyResult<f64> {
-    // Konverter nested list til Polars DataFrame
     let series_columns: Vec<Series> = data
         .into_iter()
         .enumerate()
-        .map(|(i, col)| Series::new(format!("col_{}", i).into(), col))
+        .map(|(i, col)| Series::new(format!("col_{}", i), col))
         .collect();
 
-    // Konverter Series til Columns
-    let mut columns = Vec::new();
-    for series in series_columns {
-        columns.push(series.into_column());
-    }
+    let dataframe = DataFrame::new(series_columns).map_err(|e| {
+        pyo3::exceptions::PyValueError::new_err(format!("Failed to create DataFrame: {}", e))
+    })?;
 
-    // Opprett DataFrame
-    let dataframe = DataFrame::new(columns).map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?;
-
-    // Summer alle numeriske kolonner
     let mut total_sum = 0.0;
     for column in dataframe.get_columns() {
         if let Ok(series) = column.f64() {
@@ -36,5 +29,4 @@ fn rust_dataframe_utils(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_numeric_columns, m)?)?;
     Ok(())
 }
-
 
